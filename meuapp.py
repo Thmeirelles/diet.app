5204550325797d4f76b61054166a9f0c669aa661
Thmeirelles <151373289+Thmeirelles@users.noreply.github.com>
2025-09-25 08:24:00 -0300

    Update meuapp.py

--- meuapp.py
+++ meuapp.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.slidebar(['A'])
+st.sidebar(['A'])
 st.title('DietApp') 
 st.write('Esta aplicação consegue te ajudar no cálculo de sua dieta.') 
 st.write('Antes de começar... Preciso de algumas informações suas.')
@@ -41,5 +41,6 @@ if st.session_state.confirmação_botao_1:
             else:   
 
                 st.error('Algo deu errado')
+
 
 
